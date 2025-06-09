994b2d12addd391a75c20c76913fab55734e3d83
luclima4 <lucascesarlimaral@gmail.com>
2025-06-09 17:47:42 -0300

    Favoritos ainda nao funcionando. 0 progresso.

--- public/assets/scripts/index.js
+++ public/assets/scripts/index.js
@@ -1,27 +1,43 @@
 fetch('http://localhost:3000/filmes')
-    .then(response => response.json())
-    .then(filmes => {
-        console.log("Filmes recebidos:", filmes)
+  .then(response => response.json())
+  .then(filmes => {
+    let cardsHTML = ''
 
-        let cardsHTML = ''
+    for (let i = 0; i < filmes.length; i++) {
+      let filme = filmes[i]
+      cardsHTML += `
+        <div id="filmeBanner" class="col">
+          <div id="divImagem">
+            <button id="favBtn" class="btn btn-outline-danger">
+              Favoritar
+            </button>
+            <a href="detalhes.html?id=${filme.id}">
+              <img style="border-radius: 8px;" src="${filme.imagem}" class="card-img-top" alt="...">
+            </a>
+          </div>
+          <div class="card-body align-items-center"></div>
+        </div>
+      `
+    }
 
-        for (let i = 0; i < filmes.length; i++) {
-            let filme = filmes[i]
-            cardsHTML += `
-        
-            <div id="filmeBanner" class="col">
-                <button type="button" id="heartBtn" class="btn btn-outline-danger" style="position: absolute; right: 5px;">
-                    <i class="bi bi-heart-fill"></i>
-                </button>
-                    <a href="detalhes.html?id=${filme.id}"><img style="border-radius: 8px;" src="${filme.imagem}" class="card-img-top" alt="..."></a>
-                    <div class="card-body align-items-center">
-                </div>
-            </div>
-        `
-        }
-        document.getElementById('injetaCard').innerHTML = cardsHTML
-    })
+    document.getElementById('injetaCard').innerHTML = cardsHTML
 
+  })
+
+
+/* Alterar botão não funciona */
+
+const btn = document.getElementById('favBtn')
+    btn.addEventListener('click', () => {
+
+      btn.classList.toggle('active');
+
+      if (btn.classList.contains('active')) {
+        btn.textContent = 'Favoritado';
+      } else {
+        btn.textContent = 'Favoritar';
+      }
+    });
 
 //*  ------------------------ Funções para login do usuário --------------------------------
 
@@ -29,14 +45,14 @@ fetch('http://localhost:3000/filmes')
 let usuario = null;
 
 function checarUsuarioLogado() {
-    const usuarioLogadoStr = localStorage.getItem('usuarioLogado')
+  const usuarioLogadoStr = localStorage.getItem('usuarioLogado')
 
-    if (!usuarioLogadoStr) {
-        window.location.href = 'login.html'
-        return false
-    }
-    usuario = JSON.parse(usuarioLogadoStr) 
-    return true 
+  if (!usuarioLogadoStr) {
+    window.location.href = 'login.html'
+    return false
+  }
+  usuario = JSON.parse(usuarioLogadoStr)
+  return true
 }
 
 
